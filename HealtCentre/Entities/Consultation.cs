320c6c06ab4fcca7f870c9f4ecc672a1c53915a1
Małgorzata Chudy <malgorzata.chudy@microsoft.wsei.edu.pl>
2023-07-12 00:48:19 +0200

    .

---
+++ HealtCentre/Entities/Consultation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthCentre_.Entities
+{
+    public class Consultation
+    {
+        public int Id { get; set; }
+        public int IdDoctor { get; set; }
+        public int IdPatient { get; set; }
+        public DateTime? Date { get; set; }
+        public int Diagnosis { get; set; }
+        public string Recommendation { get; set; }
+    }
+}
---
+++ HealtCentre/Entities/HealthCentreContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HealthCentre_.Entities;
+using HealthCentre.Entities;
+
+namespace HealtCentre.Entities
+{
+    public class HealthCentreContext : DbContext
+    {
+        public DbSet<Consultation> Consultations { get; set; }
+        public DbSet<Diagnosis> Diagnoses { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }       
+        public DbSet<Patient> Patients { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HealthCentreDb;Trusted_Connection=True;");
+        }
+
+
+    }
+}
